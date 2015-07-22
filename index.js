d38bf8e76c13e291f90e1ce9d8d1772be5358c3d
Masaaki Morishita <ikasama3.0@gmail.com>
2015-07-23 00:14:32 +0900

    wip

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-module.exports = function () {
+var postcss = require('postcss')
+
+function Csscs (css, rules) {
+  if (!this instanceof Csscs) {
+    return new Csscs(css, processors)
+  }
+
+  if (!rules) {
+    rules = []
+  }
+
+  this.css = css
+  this.rules = rules
+}
+
+Csscs.prototype.check = function () {
+  postcss(this.rules).process(this.css).css
 }
